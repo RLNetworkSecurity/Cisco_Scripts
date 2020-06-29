bdcd460d78e8757be2994acf13df20581d00b2fb
Cyprotect <39667645+Cyprotect@users.noreply.github.com>
2020-06-29 14:24:36 +0100

    Create tmp.py

---
+++ tmp.py
@@ -0,0 +1,12 @@
+While True:
+    username == input("enter username: ")
+    
+    password == input("enter password: ")
+    
+    if username == "Rdog" and password == "password":
+        break
+    else:
+        print("WRONG")
+    
+#Break loop print 
+print("Welcome!", username)
